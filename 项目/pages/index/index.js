965975e9a53d2348c056c95319a70611063af648
zelin <zelin@zelindeMacBook-Pro-10.local>
2020-05-20 17:21:56 +0800

    apt

--- 项目/pages/index/index.js
+++ 项目/pages/index/index.js
@@ -1,54 +1,119 @@
 //index.js
-//获取应用实例
-const app = getApp()
-
 Page({
   data: {
-    img_arr:[{"src":"http://www.linweiqin.cn/img/banner1.jpg"},
-    {"src":"http://www.linweiqin.cn/img/banner2.jpg"},
-  {"src":"http://www.linweiqin.cn/img/banner3.jpg"}
-  ]
+    swiper_list:[],
+    catItems:[],
+    floorData:[]
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  getSwiperList: function (e) {
+    const swiper_list = {
+      "message": [{
+        "image_src": "http://www.linweiqin.cn/img/banner1.jpg",
+        "goods_id": 1229,
+      }, {
+        "image_src": "http://www.linweiqin.cn/img/banner2.jpg",
+        "goods_id": 3295,
+      }, {
+        "image_src": "http://www.linweiqin.cn/img/banner3.jpg",
+        "goods_id": 382,
+      }],
+      "meta": {
+        "msg": "获取成功",
+        "status": 200
+      }
+    }
+    this.setData({
+      swiper_list: swiper_list.message
     })
+  },
+  getCatItems: function (e) {
+    const catItems = {
+      "message": [{
+        "name": "分类",
+        "image_src": "http://www.linweiqin.cn/img/cat1.png",
+        "open_type": "switchTab",
+        "navigator_url": "/pages/user/index"
+      }, {
+        "name": "秒杀拍",
+        "image_src": "http://www.linweiqin.cn/img/cat2.png",
+        "open_type": "switchTab",
+        "navigator_url": "/pages/user/index"
+      }, {
+        "name": "超市购",
+        "image_src": "http://www.linweiqin.cn/img/cat3.png",
+        "open_type": "switchTab",
+        "navigator_url": "/pages/user/index"
+      }, {
+        "name": "母婴品",
+        "image_src": "http://www.linweiqin.cn/img/cat4.png",
+        "open_type": "switchTab",
+        "navigator_url": "/pages/user/index"
+      }, {
+        "name": "母婴品",
+        "image_src": "http://www.linweiqin.cn/img/cat5.png",
+        "open_type": "switchTab",
+        "navigator_url": "/pages/user/index"
+      }],
+      "meta": {
+        "msg": "获取成功",
+        "status": 200
+      }
+    }
+    this.setData({
+      catItems: catItems.message
+    })
+  },
+  getFloorData: function (e) {
+    const floorData = {
+      "message": [{
+        "floor_title": {
+          "name": "时尚服装",
+        },
+        "product_list": [{
+          "name": "服饰",
+          "image_src": "http://www.linweiqin.cn/img/floor1.jpg",
+          "open_type": "navigate",
+          "navigator_url": "/pages/goodsList/index?query=服饰"
+        }, {
+          "name": "服饰",
+          "image_src": "http://www.linweiqin.cn/img/floor2.jpg",
+          "open_type": "navigate",
+          "navigator_url": "/pages/goodsList/index?query=服饰"
+        }, {
+          "name": "服饰",
+          "image_src": "http://www.linweiqin.cn/img/floor3.jpg",
+          "open_type": "navigate",
+          "navigator_url": "/pages/goodsList/index?query=服饰"
+        }, {
+          "name": "服饰",
+          "image_src": "http://www.linweiqin.cn/img/floor4.jpg",
+          "open_type": "navigate",
+          "navigator_url": "/pages/goodsList/index?query=服饰"
+        }, {
+          "name": "服饰",
+          "image_src": "http://www.linweiqin.cn/img/floor5.jpg",
+          "open_type": "navigate",
+          "navigator_url": "/pages/goodsList/index?query=服饰"
+        }, {
+          "name": "服饰",
+          "image_src": "http://www.linweiqin.cn/img/floor6.jpg",
+          "open_type": "navigate",
+          "navigator_url": "/pages/goodsList/index?query=服饰"
+        }]
+      }],
+      "meta": {
+        "msg": "获取成功",
+        "status": 200
+      }
+    }
+    console.log(floorData.message)
+    this.setData({
+      floorData: floorData.message
+    });
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+  this.getSwiperList()
+  this.getCatItems()
+  this.getFloorData()
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
 })
---
+++ 项目/pages/user/user.js
@@ -0,0 +1,78 @@
+// pages/user/user.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    userInfo:{}
+  },
+  getUserInfoFunc:function(e){
+    console.log(e)
+    let userInfo = e.detail.userInfo
+    this.setData({
+      userInfo:userInfo
+    })
+    //缓存登录信息
+    wx.setStorageSync("userInfo", userInfo)
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    this.setData({
+      userInfo:wx.getStorageSync("userInfo")
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
